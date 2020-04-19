aa5b3128698f8bc19574fcaec53fb0e98214aa92
someshkoli <kolisomesh27@gmail.com>
2020-04-19 23:34:44 +0530

    added spotify sdk

---
+++ lib/screens/welcome.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class WelcomePage extends StatefulWidget {
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("hello"),
+    );
+  }
+}
